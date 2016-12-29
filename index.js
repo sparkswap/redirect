5d740dbc062357b12081090afd2187eba8961898
tim neutkens <tim@weprovide.com>
2016-12-29 17:49:24 +0100

    Check if env var is set

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const { REDIRECT_URL } = process.env
 module.exports = (req ,res) => {
-  res.writeHead(301, { Location: process.env.REDIRECT_URL })
+  if (!REDIRECT_URL) {
+    return 'Please provide the REDIRECT_URL environment variable'
+  }
+
+  res.writeHead(301, { REDIRECT_URL })
   res.end()
 }
